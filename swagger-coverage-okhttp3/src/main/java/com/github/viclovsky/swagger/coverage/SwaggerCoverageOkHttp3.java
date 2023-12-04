bff6f34b3d8cc4912c02ae3f1e714926a9a0223d
U-VVM\37332 <matthias.dirickx.ext@delijn.be>
2023-12-04 16:42:40 +0100

    updated first impl and added test (copy of rest-assured)

--- swagger-coverage-okhttp3/src/main/java/com/github/viclovsky/swagger/coverage/SwaggerCoverageOkHttp3.java
+++ swagger-coverage-okhttp3/src/main/java/com/github/viclovsky/swagger/coverage/SwaggerCoverageOkHttp3.java
@@ -1,5 +1,6 @@
 package com.github.viclovsky.swagger.coverage;
 
+import okhttp3.Headers;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import io.swagger.models.Operation;
@@ -40,6 +41,8 @@ public class SwaggerCoverageOkHttp3 implements Interceptor {
         Request request = chain.request();
         HttpUrl httpUrl = request.url();
         // --> check the implementation - must extract the parameters from the path.
+        // Currently seems not feasible in a generic way.
+
         httpUrl.queryParameterNames()
             .forEach(
                 (n) -> operation.addParameter(
@@ -48,8 +51,33 @@ public class SwaggerCoverageOkHttp3 implements Interceptor {
                         .example(
                             String.join(
                                 ",",
-                                httpUrl.queryParameterValues(n))
-                        )));
-        return null;
+                                httpUrl.queryParameterValues(n)))));
+
+        // Go through values of headers and add all of those as examples
+        request.headers().names()
+            .forEach(
+                headerName -> request.headers().values(headerName)
+                    .forEach( headerValue -> operation.addParameter(
+                        new HeaderParameter()
+                            .name(headerName)
+                            .example(headerValue))));
+
+        if (Objects.nonNull(request.body())) {
+            operation.addParameter(new BodyParameter().name(BODY_PARAM_NAME));
+        }
+
+        final Response response = chain.proceed(request);
+
+        operation.addResponse(valueOf(response.code()), new io.swagger.models.Response());
+
+        Swagger swagger = new Swagger()
+            .scheme(forValue(request.url().scheme()))
+            .host(request.url().host())
+            .consumes(request.body().contentType().toString())
+            .produces(response.body().contentType().toString())
+            .path(httpUrl.url().toString(), new io.swagger.models.Path().set(request.method().toLowerCase(), operation));
+
+        writer.write(swagger);
+        return response;
     }
 }
---
+++ swagger-coverage-okhttp3/src/test/java/com/github/viclovsky/swagger/coverage/RequestLoggerInterceptorOkHttp3Test.java
@@ -0,0 +1,104 @@
+package com.github.viclovsky.swagger.coverage;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.apache.http.HttpStatus;
+import org.hamcrest.io.FileMatchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.viclovsky.swagger.coverage.SwaggerCoverageConstants.OUTPUT_DIRECTORY;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.iterableWithSize;
+
+public class RequestLoggerInterceptorOkHttp3Test {
+
+    private static final String BODY_STRING = "Hello world!";
+
+    @Rule
+    public WireMockRule mock = new WireMockRule(options().dynamicPort());
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    private List<Path> getPaths(Path path) {
+        try (Stream<Path> paths = Files.walk(path)) {
+            return paths.filter(Files::isRegularFile).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("can't walk files in swagger output directory");
+        }
+    }
+
+    @Before
+    public void setUp() {
+        configureFor(mock.port());
+        stubFor(get(anyUrl())
+                .willReturn(aResponse().withStatus(HttpStatus.SC_OK)
+                        .withBody(BODY_STRING)));
+    }
+
+    @Test
+    public void shouldCreateDefaultOutputFolder() {
+        OkHttpClient client = new OkHttpClient
+            .Builder()
+            .addInterceptor(new SwaggerCoverageOkHttp3())
+            .build();
+        Request request = new Request.Builder()
+            .url(mock.url("/hello"))
+            .build();
+        client.newCall(request);
+        assertThat(
+            Paths.get(OUTPUT_DIRECTORY).toFile(),
+            FileMatchers.anExistingDirectory());
+    }
+
+    /*
+    @Test
+    public void shouldDumpSwaggerFile() throws IOException {
+        Path output = folder.newFolder().toPath();
+
+        RestAssured.given().filter(new SwaggerCoverageRestAssured(new FileSystemOutputWriter(output)))
+                .multiPart("file", "{}")
+                .header(new Header("X-Request-ID", "h"))
+                .formParam("form_param", "f")
+                .queryParam("query_param", "q")
+                .pathParam("path_param", "p")
+                .get(mock.url("/hello/{path_param}"));
+
+        assertThat(getPaths(output), iterableWithSize(1));
+    }
+
+
+    @Test
+    public void shouldCatchExceptionRestAssuredIssue1232() throws IOException {
+        Path output = folder.newFolder().toPath();
+
+        RestAssured.given().filter(new SwaggerCoverageRestAssured(new FileSystemOutputWriter(output)))
+                .multiPart("file", "{}")
+                .header(new Header("X-Request-ID", "h"))
+                .formParam("form_param", "f", "f2")
+                .queryParam("query_param", "q", "q2")
+                .pathParam("path_param", "p")
+                .get(mock.url("/hello/{path_param}"));
+
+        assertThat(getPaths(output), iterableWithSize(1));
+    }
+     */
+}
